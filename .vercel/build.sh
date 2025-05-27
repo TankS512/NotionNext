7ffd00cb45904672d689096274a4485b44755bd3
Tank <86387729+TankS512@users.noreply.github.com>
2025-05-27 17:28:14 +0800

    Update build.sh

--- .vercel/build.sh
+++ .vercel/build.sh
@@ -3,11 +3,8 @@
 # 清除Yarn缓存
 yarn cache clean
 
-# 设置npm官方源
-yarn config set registry https://registry.npmjs.org/
-
-# 安装依赖
-yarn install --frozen-lockfile
+# 安装依赖（显式指定官方npm源）
+yarn install --frozen-lockfile --registry https://registry.npmjs.org/
 
 # 执行原有的构建命令
 yarn build
